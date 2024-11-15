8f1e8ff6dbac31ae15d3cdb686a19f161a0bdcae
BEAR0925 <aioncold99@yahoo.com.tw>
2024-11-15 16:53:14 +0800

    Initial commit

---
+++ static/script.js
@@ -0,0 +1,5 @@
+// JavaScript to trigger the ball animation
+document.addEventListener('DOMContentLoaded', function() {
+    const ball = document.querySelector('.ball');
+    ball.classList.add('animate');
+});
